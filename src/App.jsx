c092a07bef6f4a5fcfd3a72758b8bff77ac2989d
Rohit Singh <singhrohit4546@gmail.com>
2024-08-23 19:16:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,49 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
- const [latitude,setLatitude]=useState();
- const [longitude,setLongitude]=useState();
- const[adress,setadress]=useState()
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [address, setAddress] = useState('');
 
- const geo=navigator.geolocation;
+  const geo = navigator.geolocation;
 
-const watchId= geo.watchPosition(location)
- function location(position){
-  let Newlatitude=position.coords.latitude;
-  let NewLongitude=position.coords.longitude;
-  setLatitude(Newlatitude);
-  setLongitude(NewLongitude);
- }
- const getAddress= async()=>{
-  let url=`https://api.opencagedata.com/geocode/v1/json?key=2b46f105534641028b3951cf02b8c60c&q=${latitude}%2C+${longitude}&pretty=1&no_annotations=1`;
-  const loc= await fetch(url);
-  const data=await loc.json();
-  setadress(data.results[0].formatted);
- }
- function address(){
-  getAddress();
- }
+  const watchId = geo.watchPosition(location);
 
-function stopAddress(){
-  geo.clearWatch(watchId);
-  alert("Tracking Stop");
-}
+  function location(position) {
+    let newLatitude = position.coords.latitude;
+    let newLongitude = position.coords.longitude;
+    setLatitude(newLatitude);
+    setLongitude(newLongitude);
+  }
+
+  const getAddress = async () => {
+    let url = (`https://api.opencagedata.com/geocode/v1/json?key=2b46f105534641028b3951cf02b8c60c&q=${latitude}%2C+${longitude}&pretty=1&no_annotations=1`);
+    const loc = await fetch(url);
+    const data = await loc.json();
+    setAddress(data.results[0].formatted);
+  }
+
+  function fetchAddress() {
+    getAddress();
+  }
+
+  function stopAddress() {
+    geo.clearWatch(watchId);
+    alert("Tracking Stopped");
+  }
+
   return (
     <>
-     <h1>Current Location Tracking :)</h1>
-     <p>Latitude: {latitude}</p>
-     <p>Longitude: {longitude}</p>
-     <button onClick={address} className='btn-get'>Get Address</button>
-     <h3>Address : {adress}</h3>
-     <br></br>
-<button onClick={stopAddress}>Stop Tracking</button>
+      <h1>Current Location Tracking :)</h1>
+      <p>Latitude: {latitude}</p>
+      <p>Longitude: {longitude}</p>
+      <button onClick={fetchAddress} className='btn-get'>Get Address</button>
+      <h3>Address: {address}</h3>
+      <br />
+      <button onClick={stopAddress}>Stop Tracking</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
